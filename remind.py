b15c3763502ceea1a1acf46f852496449f8ae66c
Isaiah Mann <isaiah@truex.com>
2019-02-22 15:57:45 -0800

    Support reminder inline

--- remind.py
+++ remind.py
@@ -4,6 +4,7 @@ import re
 
 file_name = "reminders.txt"
 prompt_message = "What can I do for you? create(c), print(p), quit(q):\n"
+create_reminder_regex = re.compile("c.*|create.*")
 
 def get_file(file_name):
     file_path = os.getcwd() + "/" + file_name
@@ -11,9 +12,13 @@ def get_file(file_name):
         subprocess.call(["touch", file_path])
     return open(file_path, "a+")
 
-def create_reminder(reminder_file):
-    user_input = raw_input("What's your reminder?\n")
-    reminder_file.write(user_input + "\n")
+def create_reminder(reminder_file, user_input):
+    split_input = user_input.split(" ")
+    if len(split_input) > 1:
+        reminder = " ".join(split_input[1:len(split_input)])
+    else:
+        reminder = raw_input("What's your reminder?\n")
+    reminder_file.write(reminder + "\n")
     reminder_file.flush()
 
 def print_reminders(reminder_file):
@@ -32,8 +37,8 @@ def main():
     reminder_file = get_file(file_name)
     user_input = raw_input("Welcome to Reminders. " + prompt_message)
     while user_input != "quit" and user_input != "q":
-        if user_input == "create" or user_input == "c":
-            create_reminder(reminder_file)
+        if create_reminder_regex.match(user_input):
+            create_reminder(reminder_file, user_input)
         elif user_input == "print" or user_input == "p":
             print_reminders(reminder_file)
         user_input = raw_input(prompt_message)
