3114f86c2afdc069338e42cd05e07f328d4b8e13
Isaiah Mann <isaiah@truex.com>
2019-05-10 21:37:53 -0700

    Support delete reminders by index

--- remind.py
+++ remind.py
@@ -2,10 +2,11 @@ import os
 import re
 
 file_name = "reminders.txt"
-prompt_message = "What can I do for you? create(c), print(p), quit(q):\n"
+prompt_message = "What can I do for you? create(c), delete(d), print(p), quit(q):\n"
 quit_regex = re.compile("quit|q")
 create_regex = re.compile("c.*|create.*")
 print_regex = re.compile("print|p")
+del_regex = re.compile("d.*|delete.*")
 
 def create_reminder(reminder_file, user_input):
     split_input = user_input.split(" ")
@@ -16,9 +17,41 @@ def create_reminder(reminder_file, user_input):
     reminder_file.write(reminder + "\n")
     reminder_file.flush()
 
-def print_reminders(reminder_file):
+def delete_reminder(reminder_file, user_input):
     reminder_file.seek(0)
+    line_count = len(reminder_file.readlines())
+    if line_count == 0:
+        print "There are no reminders to delete"
+        return
+    split_input = user_input.split(" ")
+    del_index = None
+    prompt = "What's the index of the reminder you want to delete?\n"
+    if len(split_input) > 1:
+        del_index = int(split_input[1])
+    while type(del_index) != type(int()) or del_index > line_count or del_index < 1:
+        try:
+            del_index = int(raw_input(prompt))
+            assert del_index <= line_count
+            assert del_index > 0
+        except ValueError:
+            prompt = "Please enter a valid number\n"
+        except AssertionError:
+            prompt = "Please enter an index between 1 and {}\n".format(line_count)
+    reminder_file.seek(0)
+    lines = reminder_file.readlines()
+    del_line = lines.pop(del_index - 1).rstrip()
+    print "Deleting reminder '{}'".format(del_line)
+    reminder_file.truncate(0)
+    reminder_file.write("".join(lines))
+
+def print_reminders(reminder_file):
     print ""
+    reminder_file.seek(0)
+    line_count = len(reminder_file.readlines())
+    if line_count == 0:
+        print "You have no reminders :)"
+        return
+    reminder_file.seek(0)
     print "========="
     print "REMINDERS"
     print "========="
@@ -26,7 +59,6 @@ def print_reminders(reminder_file):
     for line in reminder_file:
         print ("(" + str(counter) + ") " + line.rstrip())
         counter += 1
-    print ""
 
 def main():
     try:
@@ -35,8 +67,11 @@ def main():
         while not(quit_regex.match(user_input) and quit_regex.match(user_input).group(0) == user_input):
             if create_regex.match(user_input):
                 create_reminder(reminder_file, user_input)
+            elif del_regex.match(user_input) and del_regex.match(user_input).group(0) == user_input:
+                delete_reminder(reminder_file, user_input)
             elif print_regex.match(user_input) and print_regex.match(user_input).group(0) == user_input:
                 print_reminders(reminder_file)
+            print ""
             user_input = raw_input(prompt_message)
     except KeyboardInterrupt:
         print ""
