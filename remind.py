11b6bf8f984c7180247991698ab880255af8bc75
Isaiah Mann <isaiahbmann@gmail.com>
2019-12-27 21:58:27 -0500

    Support delete via reminder match

--- remind.py
+++ remind.py
@@ -17,35 +17,52 @@ def create_reminder(reminder_file, user_input):
     reminder_file.write(reminder + "\n")
     reminder_file.flush()
 
+def invalid_index(idx, line_count):
+    return (type(idx) != type(int()) or
+            idx > line_count or
+            idx < 1)
+
 def delete_reminder(reminder_file, user_input):
     reminder_file.seek(0)
-    line_count = len(reminder_file.readlines())
+    lines = reminder_file.readlines()
+    line_count = len(lines)
     if line_count == 0:
         print "There are no reminders to delete"
         return
-    split_input = user_input.split(" ")
+    split_input = user_input.split(" ", 1)
     del_index = None
-    prompt = "What's the index of the reminder you want to delete?\n"
     if len(split_input) > 1:
-        del_index = int(split_input[1])
-    while type(del_index) != type(int()) or del_index > line_count or del_index < 1:
         try:
-            del_index = int(raw_input(prompt))
+            del_index = int(split_input[1])
+        except ValueError:
+            for i in range(0, line_count):
+                if split_input[1] == lines[i].rstrip():
+                    del_index = i + 1
+            if not del_index:
+                print("Couldn't find reminder '" + split_input[1] + "'")
+    prompt = "What's index of the reminder you want to delete? ('c' to cancel)\n"
+    if invalid_index(del_index, line_count):
+        print_reminders(reminder_file)
+        print
+    while invalid_index(del_index, line_count):
+        user_input = raw_input(prompt)
+        if user_input == "c":
+            return
+        try:
+            del_index = int(user_input)
             assert del_index <= line_count
             assert del_index > 0
         except ValueError:
-            prompt = "Please enter a valid number\n"
+            prompt = "Please enter a valid number ('c' to cancel)\n"
         except AssertionError:
-            prompt = "Please enter an index between 1 and {}\n".format(line_count)
-    reminder_file.seek(0)
-    lines = reminder_file.readlines()
+            prompt = "Please enter an index between 1 and {} ('c' to cancel)\n".format(line_count)
     del_line = lines.pop(del_index - 1).rstrip()
     print "Deleting reminder '{}'".format(del_line)
     reminder_file.truncate(0)
     reminder_file.write("".join(lines))
 
 def print_reminders(reminder_file):
-    print ""
+    print
     reminder_file.seek(0)
     line_count = len(reminder_file.readlines())
     if line_count == 0:
@@ -71,10 +88,10 @@ def main():
                 delete_reminder(reminder_file, user_input)
             elif print_regex.match(user_input) and print_regex.match(user_input).group(0) == user_input:
                 print_reminders(reminder_file)
-            print ""
+            print
             user_input = raw_input(prompt_message)
     except KeyboardInterrupt:
-        print ""
+        print
     finally:
         print "Goodbye!"
         reminder_file.close()
